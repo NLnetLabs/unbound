1134565dec01ee3954b9d0ccf4af1529860cd422
Wouter Wijngaards <wouter@nlnetlabs.nl>
2007-01-04 13:36:11 +0000

    Nicer code that checks correctly for needed programs..
    git-svn-id: file:///svn/unbound/trunk@14 be551aaa-1e26-0410-a405-d3ace91eadb9

--- testcode/do-tests.sh
+++ testcode/do-tests.sh
@@ -7,16 +7,12 @@ cd testdata;
 for test in `ls *.tpkg`; do
 	SKIP=0
 	if echo $NEED_SPLINT | grep $test >/dev/null; then
-		if which splint >/dev/null 2>&1; then
-			:
-		else
+		if test ! -x "`which splint`"; then
 			SKIP=1;
 		fi
 	fi
 	if echo $NEED_DOXYGEN | grep $test >/dev/null; then
-		if which doxygen >/dev/null 2>&1; then
-			:
-		else
+		if test ! -x "`which doxygen`"; then
 			SKIP=1;
 		fi
 	fi
