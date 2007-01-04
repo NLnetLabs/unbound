1c2cc969eb3d3cdb82afed07b605a974f12ea29d
Wouter Wijngaards <wouter@nlnetlabs.nl>
2007-01-04 13:32:49 +0000

    check for needed programs before running tests.
    git-svn-id: file:///svn/unbound/trunk@13 be551aaa-1e26-0410-a405-d3ace91eadb9

--- testcode/do-tests.sh
+++ testcode/do-tests.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
+NEED_SPLINT='00-lint.tpkg'
+NEED_DOXYGEN='01-doc.tpkg'
+
 cd testdata;
 for test in `ls *.tpkg`; do
-	echo $test
-	tpkg -a ../.. exe $test
+	SKIP=0
+	if echo $NEED_SPLINT | grep $test >/dev/null; then
+		if which splint >/dev/null 2>&1; then
+			:
+		else
+			SKIP=1;
+		fi
+	fi
+	if echo $NEED_DOXYGEN | grep $test >/dev/null; then
+		if which doxygen >/dev/null 2>&1; then
+			:
+		else
+			SKIP=1;
+		fi
+	fi
+	if test $SKIP -eq 0; then
+		echo $test
+		tpkg -a ../.. exe $test
+	else
+		echo "skip $test"
+	fi
 done
