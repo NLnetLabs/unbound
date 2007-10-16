631ccb58bf3409674d93a2ef978a0e87bd973496
Wouter Wijngaards <wouter@nlnetlabs.nl>
2007-10-16 14:39:27 +0000

    size_t no underflow.
    git-svn-id: file:///svn/unbound/trunk@681 be551aaa-1e26-0410-a405-d3ace91eadb9

--- util/data/msgreply.c
+++ util/data/msgreply.c
@@ -529,8 +529,9 @@ msgreply_sizefunc(void* k, void* d)
 	struct msgreply_entry* q = (struct msgreply_entry*)k;
 	struct reply_info* r = (struct reply_info*)d;
 	size_t s = sizeof(struct msgreply_entry) + sizeof(struct reply_info)
-		+ q->key.qname_len + lock_get_mem(&q->entry.lock);
-	s += (r->rrset_count-1) * sizeof(struct rrset_ref);
+		+ q->key.qname_len + lock_get_mem(&q->entry.lock)
+		- sizeof(struct rrset_ref);
+	s += r->rrset_count * sizeof(struct rrset_ref);
 	s += r->rrset_count * sizeof(struct ub_packed_rrset_key*);
 	return s;
 }
