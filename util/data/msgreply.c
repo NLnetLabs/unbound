85af59c734a2f7c2a761e0b74c40234d85398fd2
Wouter Wijngaards <wouter@nlnetlabs.nl>
2008-03-04 11:00:49 +0000

    Fixup trust for CNAME chains. (and also DNAME).
    git-svn-id: file:///svn/unbound/trunk@1013 be551aaa-1e26-0410-a405-d3ace91eadb9

--- util/data/msgreply.c
+++ util/data/msgreply.c
@@ -292,7 +292,25 @@ get_rrset_trust(struct msg_parse* msg, struct rrset_parse* rrset)
 {
 	uint16_t AA = msg->flags & BIT_AA;
 	if(rrset->section == LDNS_SECTION_ANSWER) {
-		if(AA)	return rrset_trust_ans_AA;
+		if(AA) {
+			/* RFC2181 says remainder of CNAME chain is nonauth*/
+			if(msg->rrset_first && 
+				msg->rrset_first->section==LDNS_SECTION_ANSWER
+				&& msg->rrset_first->type==LDNS_RR_TYPE_CNAME){
+				if(rrset == msg->rrset_first)
+					return rrset_trust_ans_AA;
+				else 	return rrset_trust_ans_noAA;
+			}
+			if(msg->rrset_first && 
+				msg->rrset_first->section==LDNS_SECTION_ANSWER
+				&& msg->rrset_first->type==LDNS_RR_TYPE_DNAME){
+				if(rrset == msg->rrset_first ||
+				   rrset == msg->rrset_first->rrset_all_next)
+					return rrset_trust_ans_AA;
+				else 	return rrset_trust_ans_noAA;
+			}
+			return rrset_trust_ans_AA;
+		}
 		else	return rrset_trust_ans_noAA;
 	} else if(rrset->section == LDNS_SECTION_AUTHORITY) {
 		if(AA)	return rrset_trust_auth_AA;
