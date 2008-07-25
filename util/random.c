d452ba59b57e45ba64d06176094d3e104d16a290
Wouter Wijngaards <wouter@nlnetlabs.nl>
2008-07-25 05:32:52 +0000

    Added copyright statement from OpenBSD authors.
    git-svn-id: file:///svn/unbound/trunk@1165 be551aaa-1e26-0410-a405-d3ace91eadb9

--- util/random.c
+++ util/random.c
@@ -40,7 +40,22 @@
  *
  * The code in this file is based on arc4random from
  * openssh-4.0p1/openbsd-compat/bsd-arc4random.c
- * That code is also BSD licensed.
+ * That code is also BSD licensed. Here is their statement:
+ *
+ * Copyright (c) 1996, David Mazieres <dm@uun.org>
+ * Copyright (c) 2008, Damien Miller <djm@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 #include "config.h"
 #include "util/random.h"
