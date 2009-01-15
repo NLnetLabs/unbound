45abfa8720aa895c0020085f6de955549ff14a3a
Wouter Wijngaards <wouter@nlnetlabs.nl>
2009-01-15 13:48:03 +0000

    fix libevent-1.3 and older bug
    git-svn-id: file:///svn/unbound/trunk@1441 be551aaa-1e26-0410-a405-d3ace91eadb9

--- util/netevent.c
+++ util/netevent.c
@@ -169,12 +169,14 @@ comm_base_create(int sigs)
 		return NULL;
 	}
 	comm_base_now(b);
+	/* avoid event_get_method call which causes crashes even when
+	 * not printing, because its result is passed */
 	verbose(VERB_ALGO, "libevent %s uses %s method.", 
 		event_get_version(), 
 #ifdef HAVE_EVENT_BASE_GET_METHOD
 		event_base_get_method(b->eb->base)
 #else
-		event_get_method()
+		"not_obtainable"
 #endif
 	);
 	return b;
