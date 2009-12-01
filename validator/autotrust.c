4d1c4c8002a3652aedb055c63af94b1f3b03b1a9
Wouter Wijngaards <wouter@nlnetlabs.nl>
2009-12-01 08:12:00 +0000

    Fix crash reported on unbound-users with module-config "iterator"
    git-svn-id: file:///svn/unbound/trunk@1924 be551aaa-1e26-0410-a405-d3ace91eadb9

--- validator/autotrust.c
+++ validator/autotrust.c
@@ -100,6 +100,8 @@ size_t
 autr_get_num_anchors(struct val_anchors* anchors)
 {
 	size_t res = 0;
+	if(!anchors)
+		return 0;
 	lock_basic_lock(&anchors->lock);
 	if(anchors->autr)
 		res = anchors->autr->probe.count;
