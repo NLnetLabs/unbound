62228ae4504f5ce2e3f750596021ff8e67c7eacf
Wouter Wijngaards <wouter@nlnetlabs.nl>
2010-03-16 15:19:06 +0000

    fix header
    git-svn-id: file:///svn/unbound/trunk@2042 be551aaa-1e26-0410-a405-d3ace91eadb9

--- util/netevent.h
+++ util/netevent.h
@@ -108,10 +108,10 @@ struct comm_reply {
 #ifdef IPV6_PKTINFO
 		struct in6_pktinfo v6info;
 #endif
-#ifdef IP_RECVDSTADDR
-		struct in_addr v4addr;
-#elif defined(IP_PKTINFO)
+#ifdef IP_PKTINFO
 		struct in_pktinfo v4info;
+#elif defined(IP_RECVDSTADDR)
+		struct in_addr v4addr;
 #endif
 	} 	
 		/** variable with return source data */
