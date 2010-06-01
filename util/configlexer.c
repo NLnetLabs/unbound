d57466c316eead3f6399aebd97563d08c2a8ba81
Wouter Wijngaards <wouter@nlnetlabs.nl>
2010-06-01 13:47:11 +0000

    Autoconf 2.65
    git-svn-id: file:///svn/unbound/trunk@2128 be551aaa-1e26-0410-a405-d3ace91eadb9

--- util/configlexer.c
+++ util/configlexer.c
@@ -1592,7 +1592,7 @@ static int input (void );
 /* This used to be an fputs(), but since the string might contain NUL's,
  * we now use fwrite().
  */
-#define ECHO fwrite( yytext, yyleng, 1, yyout )
+#define ECHO do { if (fwrite( yytext, yyleng, 1, yyout )) {} } while (0)
 #endif
 
 /* Gets input and stuffs it into "buf".  number of characters read, or YY_NULL,
