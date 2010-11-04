d748bca7de4dc43aaa9e2880ae165b44e2476c01
Wouter Wijngaards <wouter@nlnetlabs.nl>
2010-11-04 11:38:23 +0000

    use central entropy to seed threads.
    git-svn-id: file:///svn/unbound/trunk@2330 be551aaa-1e26-0410-a405-d3ace91eadb9

--- daemon/worker.c
+++ daemon/worker.c
@@ -1068,7 +1068,7 @@ worker_init(struct worker* worker, struct config_file *cfg,
 	seed = (unsigned int)time(NULL) ^ (unsigned int)getpid() ^
 		(((unsigned int)worker->thread_num)<<17);
 		/* shift thread_num so it does not match out pid bits */
-	if(!(worker->rndstate = ub_initstate(seed, NULL))) {
+	if(!(worker->rndstate = ub_initstate(seed, worker->daemon->rand))) {
 		seed = 0;
 		log_err("could not init random numbers.");
 		worker_delete(worker);
