af71677114f96b247b1f5ddd69be467a5cd3070f
Wouter Wijngaards <wouter@nlnetlabs.nl>
2012-03-02 11:57:53 +0000

    flush_infra remove timeout counters too.
    git-svn-id: file:///svn/unbound/trunk@2640 be551aaa-1e26-0410-a405-d3ace91eadb9

--- daemon/remote.c
+++ daemon/remote.c
@@ -1127,6 +1127,9 @@ infra_del_host(struct lruhash_entry* e, void* arg)
 	if(sockaddr_cmp(&inf->addr, inf->addrlen, &k->addr, k->addrlen) == 0) {
 		struct infra_data* d = (struct infra_data*)e->data;
 		d->probedelay = 0;
+		d->timeout_A = 0;
+		d->timeout_AAAA = 0;
+		d->timeout_other = 0;
 		rtt_init(&d->rtt);
 		if(d->ttl >= inf->now) {
 			d->ttl = inf->expired;
