319c2ba5fc72e8cbe9eabd97d4759bd3dafa0947
Wouter Wijngaards <wouter@nlnetlabs.nl>
2014-02-07 12:10:06 +0000

    add body to ifstatement if locks disabled.
    git-svn-id: file:///svn/unbound/trunk@3076 be551aaa-1e26-0410-a405-d3ace91eadb9

--- libunbound/libworker.c
+++ libunbound/libworker.c
@@ -219,8 +219,9 @@ libworker_setup(struct ub_ctx* ctx, int is_bg, struct event_base* eb)
 	if(numports == 0) {
 		int locked = !w->is_bg || w->is_bg_thread;
 		libworker_delete(w);
-		if(locked)
+		if(locked) {
 			lock_basic_unlock(&ctx->cfglock);
+		}
 		return NULL;
 	}
 	w->back = outside_network_create(w->base, cfg->msg_buffer_size,
