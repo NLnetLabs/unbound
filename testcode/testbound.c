c7d1e6ccf915416c1895e8201c2d1dbf34ae09c1
Wouter Wijngaards <wouter@nlnetlabs.nl>
2014-04-08 07:13:49 +0000

    - Fix #574: make test fails on Ubuntu 14.04.  Disabled remote-control   in testbound scripts.
    git-svn-id: file:///svn/unbound/trunk@3104 be551aaa-1e26-0410-a405-d3ace91eadb9

--- testcode/testbound.c
+++ testcode/testbound.c
@@ -193,6 +193,7 @@ setup_config(FILE* in, int* lineno, int* pass_argc, char* pass_argv[])
 	fprintf(cfg, "		username: \"\"\n");
 	fprintf(cfg, "		pidfile: \"\"\n");
 	fprintf(cfg, "		val-log-level: 2\n");
+	fprintf(cfg, "remote-control:	control-enable: no\n");
 	while(fgets(line, MAX_LINE_LEN-1, in)) {
 		parse = line;
 		(*lineno)++;
