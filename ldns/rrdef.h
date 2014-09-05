3e510bedeee14c1db0abaf281187bba8af8efb8f
Wouter Wijngaards <wouter@nlnetlabs.nl>
2014-09-05 09:25:31 +0000

    - type CDS and CDNSKEY types in sldns.
    git-svn-id: file:///svn/unbound/trunk@3220 be551aaa-1e26-0410-a405-d3ace91eadb9

--- ldns/rrdef.h
+++ ldns/rrdef.h
@@ -191,8 +191,8 @@ enum sldns_enum_rr_type
 	LDNS_RR_TYPE_RKEY = 57,
         /** draft-ietf-dnsop-trust-history */
         LDNS_RR_TYPE_TALINK = 58,
-	/** draft-barwood-dnsop-ds-publis */
-	LDNS_RR_TYPE_CDS = 59,
+	LDNS_RR_TYPE_CDS = 59, /** RFC 7344 */
+	LDNS_RR_TYPE_CDNSKEY = 60, /** RFC 7344 */
 
 	LDNS_RR_TYPE_SPF = 99, /* RFC 4408 */
 
