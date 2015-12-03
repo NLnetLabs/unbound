3328dc4d68b768dcf7af305001317dc490d7ea20
Ralph Dolmans <ralph@nlnetlabs.nl>
2015-12-03 15:45:52 +0000

    Stop minimising after receiving rcode!=NOERROR
    git-svn-id: file:///svn/unbound/trunk@3573 be551aaa-1e26-0410-a405-d3ace91eadb9

--- iterator/iterator.c
+++ iterator/iterator.c
@@ -2470,6 +2470,8 @@ processQueryResponse(struct module_qstate* qstate, struct iter_qstate* iq,
 	/* LAME, THROWAWAY and "unknown" all end up here.
 	 * Recycle to the QUERYTARGETS state to hopefully try a 
 	 * different target. */
+	if (qstate->env->cfg->qname_minimisation)
+		iq->minimisation_state = DONOT_MINIMISE_STATE;
 	return next_state(iq, QUERYTARGETS_STATE);
 }
 
