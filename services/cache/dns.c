9581c39dcc721c812750a305ee3a1d6005935dd5
Ralph Dolmans <ralph@nlnetlabs.nl>
2016-11-22 10:50:53 +0000

    - QNAME minimisation uses QTYPE=A, therefore always check cache for   this type in harden-below-nxdomain functionality.
    git-svn-id: file:///svn/unbound/trunk@3932 be551aaa-1e26-0410-a405-d3ace91eadb9

--- services/cache/dns.c
+++ services/cache/dns.c
@@ -798,9 +798,9 @@ dns_cache_lookup(struct module_env* env,
 		dname_remove_label(&k.qname, &k.qname_len);
 		h = query_info_hash(&k, flags);
 		e = slabhash_lookup(env->msg_cache, h, &k, 0);
-		if(!e && k.qtype != LDNS_RR_TYPE_NS &&
+		if(!e && k.qtype != LDNS_RR_TYPE_A &&
 			env->cfg->qname_minimisation) {
-			k.qtype = LDNS_RR_TYPE_NS;
+			k.qtype = LDNS_RR_TYPE_A;
 			h = query_info_hash(&k, flags);
 			e = slabhash_lookup(env->msg_cache, h, &k, 0);
 		}
@@ -820,7 +820,7 @@ dns_cache_lookup(struct module_env* env,
 			lock_rw_unlock(&e->lock);
 		}
 		k.qtype = qtype;
-	}
+	    }
 
 	/* fill common RR types for ANY response to avoid requery */
 	if(qtype == LDNS_RR_TYPE_ANY) {
