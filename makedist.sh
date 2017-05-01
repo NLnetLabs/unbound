f610f40558e4a7c019347a78d9cd6ed04532b39c
Wouter Wijngaards <wouter@nlnetlabs.nl>
2017-05-01 14:57:36 +0000

    Fix #1258: 64bit getting installed in C:\Program Files (x86).
    git-svn-id: file:///svn/unbound/trunk@4140 be551aaa-1e26-0410-a405-d3ace91eadb9

--- makedist.sh
+++ makedist.sh
@@ -368,6 +368,10 @@ if [ "$DOWIN" = "yes" ]; then
     info "Creating installer"
     quadversion=`cat ../config.h | grep RSRC_PACKAGE_VERSION | sed -e 's/#define RSRC_PACKAGE_VERSION //' -e 's/,/\\./g'`
     cat ../winrc/setup.nsi | sed -e 's/define VERSION.*$/define VERSION "'$version'"/' -e 's/define QUADVERSION.*$/define QUADVERSION "'$quadversion'"/' > ../winrc/setup_ed.nsi
+    if test "$W64" = "yes"; then
+	mv ../winrc/setup_ed.nsi ../winrc/setup_ed_old.nsi
+	cat ../winrc/setup_ed_old.nsi | sed -e 's/PROGRAMFILES/PROGRAMFILES64/' > ../winrc/setup_ed.nsi
+    fi
     "$makensis" ../winrc/setup_ed.nsi
     info "Created installer"
     cd ..
