4770bfbb5b6183646faa516e8da230cd7317965e
Wouter Wijngaards <wouter@nlnetlabs.nl>
2017-05-02 12:05:10 +0000

    - Fix sldns wire2str printout of RR type CAA tags.
    git-svn-id: file:///svn/unbound/trunk@4142 be551aaa-1e26-0410-a405-d3ace91eadb9

--- sldns/wire2str.c
+++ sldns/wire2str.c
@@ -1639,10 +1639,10 @@ int sldns_wire2str_tag_scan(uint8_t** d, size_t* dl, char** s, size_t* sl)
 	if(*dl < 1+n)
 		return -1;
 	for(i=0; i<n; i++)
-		if(!isalnum((unsigned char)(*d)[i]))
+		if(!isalnum((unsigned char)(*d)[i+1]))
 			return -1;
 	for(i=0; i<n; i++)
-		w += sldns_str_print(s, sl, "%c", (char)(*d)[i]);
+		w += sldns_str_print(s, sl, "%c", (char)(*d)[i+1]);
 	(*d)+=n+1;
 	(*dl)-=(n+1);
 	return w;
