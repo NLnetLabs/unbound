71788cf43a014eecca75778c972d179447fcd42d
Wouter Wijngaards <wouter@nlnetlabs.nl>
2017-07-06 13:30:37 +0000

    Fixup speed_cache for 01-doc test.
    git-svn-id: file:///svn/unbound/trunk@4265 be551aaa-1e26-0410-a405-d3ace91eadb9

--- testdata/speed_cache.tdir/makeqs.c
+++ testdata/speed_cache.tdir/makeqs.c
@@ -1,5 +1,10 @@
+/**
+ * \file
+ * This file creates queries for the speed test
+ */
 #include <stdio.h>
 #include <stdlib.h>
+/** main program to create queries, pass the number of them. */
 int main(int argc, char** argv) {
         int i;
         int max = atoi(argv[1]);
