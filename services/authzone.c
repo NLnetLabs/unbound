0d18256828dc8b48ef03830acd26168dcdb98a2a
Wouter Wijngaards <wouter@nlnetlabs.nl>
2018-04-23 10:35:35 +0000

    - Fix sldns parse failure for CDS alternate delete syntax empty hex.
    git-svn-id: file:///svn/unbound/trunk@4646 be551aaa-1e26-0410-a405-d3ace91eadb9

--- services/authzone.c
+++ services/authzone.c
@@ -4220,6 +4220,11 @@ chunkline_newline_removal(sldns_buffer* buf)
 	size_t i, end=sldns_buffer_limit(buf);
 	for(i=0; i<end; i++) {
 		char c = (char)sldns_buffer_read_u8_at(buf, i);
+		if(c == '\n' && i==end-1) {
+			sldns_buffer_write_u8_at(buf, i, 0);
+			sldns_buffer_set_limit(buf, end-1);
+			return;
+		}
 		if(c == '\n')
 			sldns_buffer_write_u8_at(buf, i, (uint8_t)' ');
 	}
