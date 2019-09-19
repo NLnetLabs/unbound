ce0e9bef453dc3209912c544a36bad920f26ae13
Maryse47 <41080948+Maryse47@users.noreply.github.com>
2019-09-19 17:35:30 +0200

    Consistently use /dev/urandom instead of /dev/random in scripts and docs
    Unbound code call /dev/urandom (see below)  but various docs and scripts
    mention /dev/random which may be confusing.
    
    https://github.com/NLnetLabs/unbound/blob/release-1.9.3/compat/arc4random.c#L107
    https://github.com/NLnetLabs/unbound/blob/release-1.9.3/compat/getentropy_linux.c#L251
    https://github.com/NLnetLabs/unbound/blob/release-1.9.3/compat/getentropy_osx.c
    https://github.com/NLnetLabs/unbound/blob/release-1.9.3/compat/getentropy_solaris.c#L116

--- dnscrypt/dnscrypt.c
+++ dnscrypt/dnscrypt.c
@@ -877,7 +877,7 @@ sodium_misuse_handler(void)
 	fatal_exit(
 		"dnscrypt: libsodium could not be initialized, this typically"
 		" happens when no good source of entropy is found. If you run"
-		" unbound in a chroot, make sure /dev/random is available. See"
+		" unbound in a chroot, make sure /dev/urandom is available. See"
 		" https://www.unbound.net/documentation/unbound.conf.html");
 }
 
