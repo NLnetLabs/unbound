51c23b02099b5c279a8459641727adb198078193
W.C.A. Wijngaards <wouter@nlnetlabs.nl>
2019-11-19 16:46:33 +0100

    - Fix Out of Bounds Read in sldns_str2wire_dname(),   reported by X41 D-Sec.

--- sldns/str2wire.c
+++ sldns/str2wire.c
@@ -172,7 +172,9 @@ uint8_t* sldns_str2wire_dname(const char* str, size_t* len)
 	uint8_t dname[LDNS_MAX_DOMAINLEN+1];
 	*len = sizeof(dname);
 	if(sldns_str2wire_dname_buf(str, dname, len) == 0) {
-		uint8_t* r = (uint8_t*)malloc(*len);
+		uint8_t* r;
+		if(*len > sizeof(dname)) return NULL;
+		r = (uint8_t*)malloc(*len);
 		if(r) return memcpy(r, dname, *len);
 	}
 	*len = 0;
