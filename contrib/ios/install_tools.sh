2e3f65603dd775fa0db76295c2694c624dd08d63
Jeffrey Walton <noloader@gmail.com>
2020-03-26 19:31:38 -0400

    Fix Travis failures due to Homebrew

--- contrib/ios/install_tools.sh
+++ contrib/ios/install_tools.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # This step should install tools needed for all packages - OpenSSL, Expat and Unbound
+# When running on Travis, Homebrew fails in unusual ways, hence '|| true'.
+# https://travis-ci.community/t/homebrew-fails-because-an-automake-update-is-an-error/7831/3
 echo "Updating tools"
-brew update 1>/dev/null
+brew update 1>/dev/null || true
 echo "Installing tools"
-# already installed are: autoconf automake libtool pkg-config
-brew install curl perl 1>/dev/null
+brew install autoconf automake libtool pkg-config curl perl 1>/dev/null || true
