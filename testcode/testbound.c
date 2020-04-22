a269db3828a107fbf7601567268b41800999e427
George Thessalonikefs <george@nlnetlabs.nl>
2020-04-22 19:27:20 +0200

    - Explicitly use 'rrset-roundrobin: no' for test cases.

--- testcode/testbound.c
+++ testcode/testbound.c
@@ -274,8 +274,6 @@ setup_config(FILE* in, int* lineno, int* pass_argc, char* pass_argv[])
 	fprintf(cfg, "		pidfile: \"\"\n");
 	fprintf(cfg, "		val-log-level: 2\n");
 	fprintf(cfg, "remote-control:	control-enable: no\n");
-	/* some basic settings to facilitate testing */
-	fprintf(cfg, "server:	rrset-roundrobin: no\n");
 	while(fgets(line, MAX_LINE_LEN-1, in)) {
 		parse = line;
 		(*lineno)++;
