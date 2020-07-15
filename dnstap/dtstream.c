7189ba715d57ef4b34c7773ee268beac2d81df49
Ralph Dolmans <ralph@nlnetlabs.nl>
2020-07-15 17:33:48 +0200

    dnstap log message typo fix

--- dnstap/dtstream.c
+++ dnstap/dtstream.c
@@ -1120,7 +1120,7 @@ static int dtio_read_accept_frame(struct dt_io_thread* dtio)
 				return 1;
 			} else {
 				/* unknow content type */
-				verbose(VERB_ALGO, "dnstap: ACCEPT frames "
+				verbose(VERB_ALGO, "dnstap: ACCEPT frame "
 					"contains unknown content type, "
 					"closing connection");
 				goto close_connection;
