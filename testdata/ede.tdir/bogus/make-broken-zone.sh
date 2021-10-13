98eb2bfdde5d6f4c71d2d5eacf9fc5d5389ff4b2
TCY16 <tom@nlnetlabs.nl>
2021-10-13 11:17:42 +0200

    add test setup for DNSSEC EDEs

---
+++ testdata/ede.tdir/bogus/make-broken-zone.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+CSK=`ldns-keygen -a ECDSAP256SHA256 -k -r /dev/urandom dnssec-failures.test`
+echo $CSK
+
+echo ". IN DS 20326 8 2 e06d44b80b8f1d39a95c0b0d7c65d08458e880409bbc683457104237c7f8ec8d" | \
+        cat $CSK.ds - > trust-anchors
+
+ONEMONTHAGO=`date -d 'now - 1 month' +%Y%m%d`
+YESTERDAY=`date -d 'now - 2 days' +%Y%m%d`
+TOMORROW=`date -d 'now + 2 days' +%Y%m%d`
+ldns-signzone -i $YESTERDAY dnssec-failures.test $CSK -f - | \
+        grep -v '^missingrrsigs\.dnssec-failures\.test\..*IN.*RRSIG.*TXT' | \
+        sed 's/Signatures invalid/Signatures INVALID/g' | \
+        grep -v '^notyetincepted\.dnssec-failures\.test\..*IN.*TXT' | \
+        grep -v '^notyetincepted\.dnssec-failures\.test\..*IN.*RRSIG.*TXT' | \
+        grep -v '^expired\.dnssec-failures\.test\..*IN.*TXT' | \
+        grep -v '^expired\.dnssec-failures\.test\..*IN.*RRSIG.*TXT' > base
+ldns-signzone -i $ONEMONTHAGO -e $YESTERDAY dnssec-failures.test $CSK -f - | \
+        grep -v '[      ]NSEC[  ]' | \
+        grep '^expired\.dnssec-failures\.test\..*IN.*TXT' > expired
+ldns-signzone -i $TOMORROW dnssec-failures.test $CSK -f - | \
+        grep -v '[      ]NSEC[  ]' | \
+        grep '^notyetincepted\.dnssec-failures\.test\..*IN.*TXT' > notyetincepted
+cat base expired notyetincepted > dnssec-failures.test.signed && rm -f base expired notyetincepted $CSK.*
