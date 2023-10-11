e98b89651e2b87f8bbf8aa061133c6748ecb02a3
George Thessalonikefs <yorgos@nlnetlabs.nl>
2023-10-11 11:44:55 +0200

    - Fix #850: [FR] Ability to use specific database in Redis, with new   redis-logical-db configuration option.

--- util/config_file.c
+++ util/config_file.c
@@ -388,6 +388,7 @@ config_create(void)
 	cfg->redis_timeout = 100;
 	cfg->redis_server_port = 6379;
 	cfg->redis_expire_records = 0;
+	cfg->redis_logical_db = 0;
 #endif  /* USE_REDIS */
 #endif  /* USE_CACHEDB */
 #ifdef USE_IPSET
@@ -1313,6 +1314,7 @@ config_get_option(struct config_file* cfg, const char* opt,
 	else O_STR(opt, "redis-server-password", redis_server_password)
 	else O_DEC(opt, "redis-timeout", redis_timeout)
 	else O_YNO(opt, "redis-expire-records", redis_expire_records)
+	else O_DEC(opt, "redis-logical-db", redis_logical_db)
 #endif  /* USE_REDIS */
 #endif  /* USE_CACHEDB */
 #ifdef USE_IPSET
