eefdbb341f19fe17d91bd5ded73357c1f33e395b
Yorgos Thessalonikefs <yorgos@nlnetlabs.nl>
2024-11-22 15:30:51 +0100

    - Fix #1175: serve-expired does not adhere to secure-by-default   principle. The default value of serve-expired-client-timeout   is set to 1800 as suggested by RFC8767.

--- util/config_file.c
+++ util/config_file.c
@@ -283,7 +283,7 @@ config_create(void)
 	cfg->serve_expired_ttl = 0;
 	cfg->serve_expired_ttl_reset = 0;
 	cfg->serve_expired_reply_ttl = 30;
-	cfg->serve_expired_client_timeout = 0;
+	cfg->serve_expired_client_timeout = 1800;
 	cfg->ede_serve_expired = 0;
 	cfg->serve_original_ttl = 0;
 	cfg->zonemd_permissive_mode = 0;
