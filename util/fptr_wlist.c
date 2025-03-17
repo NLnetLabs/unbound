42d33eb1031783ca4bfdd2dc4bc67d11090af5b4
Martine S. Lenders <martine.lenders@tu-dresden.de>
2025-03-17 12:42:39 +0700

    Restructure DoC code and initialize based on CoAP ports config
    ... And reverting most API changes this way.

--- util/fptr_wlist.c
+++ util/fptr_wlist.c
@@ -173,7 +173,6 @@ int
 fptr_whitelist_event(void (*fptr)(int, short, void *))
 {
 	if(fptr == &comm_point_udp_callback) return 1;
-	else if(fptr == &comm_point_oscore_callback) return 1;
 #if defined(AF_INET6) && defined(IPV6_PKTINFO) && defined(HAVE_RECVMSG)
 	else if(fptr == &comm_point_udp_ancil_callback) return 1;
 #endif
@@ -188,6 +187,9 @@ fptr_whitelist_event(void (*fptr)(int, short, void *))
 	else if(fptr == &comm_point_http_handle_callback) return 1;
 #ifdef HAVE_NGTCP2
 	else if(fptr == &comm_point_doq_callback) return 1;
+#endif
+#ifdef HAVE_COAP
+	else if(fptr == &comm_point_doc_callback) return 1;
 #endif
 #ifdef USE_DNSTAP
 	else if(fptr == &dtio_output_cb) return 1;
