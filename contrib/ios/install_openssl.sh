6a5385f291e9cccd86340e7a5feb73c948bbd1e8
W.C.A. Wijngaards <wouter@nlnetlabs.nl>
2025-10-15 12:25:44 +0200

    - Fix to update openssl version in ios ci.

--- contrib/ios/install_openssl.sh
+++ contrib/ios/install_openssl.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
+OPENSSL_VERSION=3.0.18
 
 echo "Downloading OpenSSL"
-if ! curl -L -k -s -o openssl-1.1.1d.tar.gz https://www.openssl.org/source/openssl-1.1.1d.tar.gz;
+if ! curl -L -k -s -o openssl-$OPENSSL_VERSION.tar.gz https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz;
 then
     echo "Failed to download OpenSSL"
     exit 1
 fi
 
 echo "Unpacking OpenSSL"
-rm -rf ./openssl-1.1.1d
-if ! tar -xf openssl-1.1.1d.tar.gz;
+rm -rf ./openssl-$OPENSSL_VERSION
+if ! tar -xf openssl-$OPENSSL_VERSION.tar.gz;
 then
     echo "Failed to unpack OpenSSL"
     exit 1
 fi
 
-cd openssl-1.1.1d || exit 1
+cd openssl-$OPENSSL_VERSION || exit 1
 
 if ! cp ../contrib/ios/15-ios.conf Configurations/; then
     echo "Failed to copy OpenSSL ios config"
@@ -27,11 +28,12 @@ fi
 # Also see https://github.com/openssl/openssl/issues/7607.
 if ! patch -u -p0 < ../contrib/ios/openssl.patch; then
     echo "Failed to patch OpenSSL"
-    exit 1
+    # the partial patch may be useful.
+    #exit 1
 fi
 
 echo "Configuring OpenSSL"
-if ! ./Configure "$OPENSSL_HOST" -DNO_FORK no-comp no-asm no-hw no-engine no-tests no-unit-test \
+if ! ./Configure "$OPENSSL_HOST" -DNO_FORK no-comp no-asm no-engine no-tests no-unit-test \
        --prefix="$IOS_PREFIX" --openssldir="$IOS_PREFIX"; then
     echo "Failed to configure OpenSSL"
     exit 1
